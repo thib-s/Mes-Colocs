98e273e763eb7a5f9835b259370fd55439de724e
Thibaut Boissin <thibaut.boissin@gmail.com>
2017-01-12 13:28:17 +0100

    Revert "ajout persistence coloc"
    This reverts commit b0eab8d0c452a9890004d76026097a2c5369ca02.

--- PR-Web/src/controller/CreateColocServlet.java
+++ PR-Web/src/controller/CreateColocServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Base64;
 
 import javax.ejb.EJB;
-import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -25,7 +24,6 @@ public class CreateColocServlet extends HttpServlet {
 
 	@EJB 
 	model.Facade facade;
-	private EntityManager entityManager;
 	/**
 	 * Default constructor.
 	 */
@@ -56,7 +54,6 @@ public class CreateColocServlet extends HttpServlet {
 					Coloc coloc = facade.addColoc(user, cName, address, city, country, b64encode.getEncoder().encodeToString(pass.getBytes()));
 					facade.bindUserColoc(user,coloc);
 					user = facade.getUser(user.getUsername());
-					entityManager.persist(coloc);
 					session.setAttribute("sessionUser", user);
 					request.getRequestDispatcher("homeColoc.jsp").forward(request,response);
 				} else {
