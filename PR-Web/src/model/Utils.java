d5203ef44f115db540a36971e7d280e7b8cc6cf6
Fxstempfel <fxstempfel@gmail.com>
2017-01-14 20:31:13 +0100

    ajout utils

---
+++ PR-Web/src/model/Utils.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.util.Calendar;
+
+public class Utils {
+
+	public static String intToDay(int num) {
+		switch (num) {
+			case Calendar.SUNDAY:
+				return "Sunday";
+			case Calendar.MONDAY:
+				return "Monday";
+			case Calendar.TUESDAY:
+				return "Tuesday";
+			case Calendar.WEDNESDAY:
+				return "Wednesday";
+			case Calendar.THURSDAY:
+				return "Thursday";
+			case Calendar.FRIDAY:
+				return "Friday";
+			case Calendar.SATURDAY:
+				return "Saturday";
+			default:
+				return "Error";
+		}
+	}
+	
+	public static String intToMonth(int num) {
+		switch (num) {
+			case Calendar.JANUARY:
+				return "January";
+			case Calendar.FEBRUARY:
+				return "February";
+			case Calendar.MARCH:
+				return "March";
+			case Calendar.APRIL:
+				return "April";
+			case Calendar.MAY:
+				return "May";
+			case Calendar.JUNE:
+				return "June";
+			case Calendar.JULY:
+				return "July";
+			case Calendar.AUGUST:
+				return "August";
+			case Calendar.SEPTEMBER:
+				return "September";
+			case Calendar.OCTOBER:
+				return "October";
+			case Calendar.NOVEMBER:
+				return "November";
+			case Calendar.DECEMBER:
+				return "December";
+			default:
+				return "Error";
+		}
+	}
+	
+}
